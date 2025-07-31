9155fdca36d45bc7e48dff22ac65497ca553e25b
Kenneth Davidson <xylene2016@protonmail.com>
2025-07-31 05:57:02 -0400

    Update run-build-al9.sh

--- run-build-al9.sh
+++ run-build-al9.sh
@@ -21,7 +21,7 @@ docker exec al9 dnf install -y wget bash
 docker exec al9 /bin/bash -c 'ln -svf bash /bin/sh'
 docker exec al9 /bin/bash -c 'rm -fr /tmp/*'
 docker cp al9 al9:/home/
-docker exec al9 /bin/bash /home/al9/install-kernel.sh
+#docker exec al9 /bin/bash /home/al9/install-kernel.sh
 docker exec al9 /bin/bash /home/al9/.preinstall_al9
 docker exec al9 /bin/bash /home/al9/build-keepalived.sh
 _keepalived_ver="$(docker exec al9 ls -1 /tmp/ | grep -i '^keepalived.*xz$' | sed -e 's|keepalived-||g' -e 's|-[0-1].el.*||g')"
